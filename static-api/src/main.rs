55bf36d009a1c916a347b202cf235b2ca6355dd5
MattDESTROYER <mattdestroyerpro@gmail.com>
2025-12-20 16:21:16 +0000

    Fix 404 errors + remove blocking file read on every 404

--- static-api/src/main.rs
+++ static-api/src/main.rs
@@ -1,18 +1,25 @@
 use std::{
     fs,
-    net::SocketAddr
+    net::SocketAddr,
+    sync::Arc
 };
 
 use tokio::net::TcpListener;
 
 use axum::{
+    body::Body,
+    http::{
+        Request,
+        StatusCode
+    },
+    middleware::{
+        self,
+        Next
+    },
     response::{
         Html,
-        IntoResponse
-    },
-    routing::{
-        get_service,
-        MethodRouter
+        IntoResponse,
+        Response
     },
     Router
 };
@@ -29,20 +36,21 @@ const PORT: u16 = 3002; // static.matthewjames.xyz
 
 #[tokio::main]
 async fn main() {
-    let serve_dir: MethodRouter = get_service(ServeDir::new("./static"))
-        .handle_error(|_| async {
-            match fs::read_to_string("./static/404.html") {
-                Ok(contents) => Html(contents).into_response(),
-                Err(_) => (axum::http::StatusCode::NOT_FOUND, "404 Not Found").into_response()
-            }
-        });
+    let not_found_html = Arc::new(
+        fs::read_to_string("./static/404.html")
+            .unwrap_or_else(|_| "<h1>404 Not Found</h1>".to_string())
+    );
 
     let cors_layer: CorsLayer = CorsLayer::new()
+        .allow_methods(Any)
         .allow_origin(Any);
 
     let app = Router::new()
-        .fallback_service(serve_dir)
-        .layer(cors_layer);
+        .layer(cors_layer)
+        .fallback_service(ServeDir::new("./static"))
+        .layer(middleware::from_fn(move |req, next| {
+            custom_404_handler(req, next, not_found_html.clone())
+        }));
 
     let listener: TcpListener = tokio::net::TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], PORT)))
         .await
@@ -51,4 +59,14 @@ async fn main() {
     axum::serve(listener, app)
         .await
         .unwrap();
+}
+
+async fn custom_404_handler(req: Request<Body>, next: Next, html: Arc<String>) -> Response {
+    let response = next.run(req).await;
+
+    if response.status() == StatusCode::NOT_FOUND {
+        return (StatusCode::NOT_FOUND, Html(html.as_str().to_string())).into_response();
+    }
+
+    response
 }
